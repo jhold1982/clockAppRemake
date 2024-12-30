6e9305a1c4d449f0533c862bbeefa0a2b9a3488c
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-12-30 12:30:26 -0600

    Initial Commit

---
+++ clockAppRemake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clockAppRemake
+//
+//  Created by Justin Hold on 12/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clockAppRemake/clockAppRemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockAppRemakeApp.swift
+//  clockAppRemake
+//
+//  Created by Justin Hold on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct clockAppRemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
